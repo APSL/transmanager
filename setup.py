e37e1deccf1b32018da8b51c708edfa493c5a0bc
Andreu Vallbona <avallbona@apsl.net>
2016-06-03 15:52:22 +0200

    updated version number

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='transmanager',
-    version='0.2',
+    version='0.2.0',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
