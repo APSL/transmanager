e79c88d2fb066ec722c15103b15c9f8f89a8f775
Andreu Vallbona <avallbona@apsl.net>
2016-07-20 15:53:03 +0200

    deactivated jquery chosen

--- transmanager/static/transmanager/js/main.js
+++ transmanager/static/transmanager/js/main.js
@@ -1,19 +1,26 @@
 $(document).ready(function(){
 
     // Para los selects
-    $('select').chosen({});
+    // $('select').chosen({});
+
     $('body').on('click', 'li.tab-pane > a', function(e){
-        $('.chosen-container').css('width', '100%');
+        // $('.chosen-container').css('width', '100%');
         e.preventDefault();
     });
 
     // reset form
+    // $('body').on('click', '.reset-filters', function(e){
+    //     e.preventDefault();
+    //     $(this).closest('form').
+    //         find("input[type=text]").val("").end().
+    //         find("select").val('').trigger('liszt:updated').end().
+    //         submit();
+    // });
+
+    // reset form
     $('body').on('click', '.reset-filters', function(e){
         e.preventDefault();
-        $(this).closest('form').
-            find("input[type=text]").val("").end().
-            find("select").val('').trigger('liszt:updated').end().
-            submit();
+        $(this).closest('form').find("input[type=text]").val("").end().find("select").val('').end().submit();
     });
 
 });
